d6c11bb1b6902c755ce79da170b002e035df8a4d
Doobii <98264888+Doobiii@users.noreply.github.com>
2024-03-13 03:17:42 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a
-              href="#"
+              href="https://github.com/Doobiii/Portfolio.io"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               Github
